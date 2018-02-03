95dbc81eb34ad9d0ee322880617ff9ea4cafcbde
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-02-03 15:37:46 -0500

    new codes

--- quizizz/assets/codes.js
+++ quizizz/assets/codes.js
@@ -3,159 +3,159 @@
 		error = "true";
 		
 		// mutlimedia
-			if (game == "multimedia-1") { // lesson 1
+			if (game == "multimedia-1") {   // lesson 1
+				error = "false";
+				return "137633";
+			} else
+			if (game == "multimedia-2") {   // lesson 2
 				error = "false";
 				return "000000";
 			} else
-			if (game == "multimedia-2") { // lesson 2
+			if (game == "multimedia-r1") {   // lesson v1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "multimedia-r1") { // lesson v1
-				error = "false";
-				return "000000";
-			} else
-			if (game == "multimedia-r2") { // review v2
+			if (game == "multimedia-r2") {   // review v2
 				error = "false";
 				return "000000";
 			} else
 		
 		// programming
-			if (game == "programming-1") { // lesson 1
+			if (game == "programming-1") {   // lesson 1
 				error = "false";
 				return "000001";
 			} else
-			if (game == "programming-2") { // lesson 2
+			if (game == "programming-2") {   // lesson 2
 				error = "false";
 				return "000000";
 			} else
-			if (game == "programming-3") { // lesson 3
+			if (game == "programming-3") {   // lesson 3
 				error = "false";
 				return "000000";
 			} else
-			if (game == "programming-4") { // lesson 4
+			if (game == "programming-4") {   // lesson 4
 				error = "false";
 				return "000000";
 			} else
-			if (game == "programming-5") { // lesson 5
+			if (game == "programming-5") {   // lesson 5
 				error = "false";
 				return "000000";
 			} else
-			if (game == "programming-6") { // lesson 6
+			if (game == "programming-6") {   // lesson 6
 				error = "false";
 				return "000000";
 			} else
-			if (game == "programming-r1") { // lesson v1
+			if (game == "programming-r1") {   // lesson v1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "programming-r2") { // review v2
+			if (game == "programming-r2") {   // review v2
 				error = "false";
 				return "000000";
 			} else
 		
 		// web design
-			if (game == "webdesign-1") { // lesson 1
+			if (game == "webdesign-1") {   // lesson 1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "webdesign-2") { // lesson 2
+			if (game == "webdesign-2") {   // lesson 2
 				error = "false";
 				return "000000";
 			} else
-			if (game == "webdesign-3") { // lesson 3
+			if (game == "webdesign-3") {   // lesson 3
 				error = "false";
 				return "000000";
 			} else
-			if (game == "webdesign-4") { // lesson 4
+			if (game == "webdesign-4") {   // lesson 4
 				error = "false";
 				return "000000";
 			} else
-			if (game == "webdesign-r1") { // lesson v1
+			if (game == "webdesign-r1") {   // lesson v1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "webdesign-r2") { // review v2
+			if (game == "webdesign-r2") {   // review v2
 				error = "false";
 				return "000000";
 			} else
 		
 		// gaming
-			if (game == "gaming-1") { // lesson 1
+			if (game == "gaming-1") {   // lesson 1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "gaming-2") { // lesson 2
+			if (game == "gaming-2") {   // lesson 2
 				error = "false";
 				return "000000";
 			} else
-			if (game == "gaming-3") { // lesson 3
+			if (game == "gaming-3") {   // lesson 3
 				error = "false";
 				return "000000";
 			} else
-			if (game == "gaming-4") { // lesson 4
+			if (game == "gaming-4") {   // lesson 4
 				error = "false";
 				return "000000";
 			} else
-			if (game == "gaming-r1") { // review v1
+			if (game == "gaming-r1") {   // review v1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "gaming-r2") { // review v2
+			if (game == "gaming-r2") {   // review v2
 				error = "false";
 				return "000000";
 			} else
 			
 		// iba
-			if (game == "iba-1") { // lesson 1
+			if (game == "iba-1") {   // lesson 1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-2") { // lesson 2
+			if (game == "iba-2") {   // lesson 2
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-3") { // lesson 3
+			if (game == "iba-3") {   // lesson 3
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-4") { // lesson 4
+			if (game == "iba-4") {   // lesson 4
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-5") { // lesson 5
+			if (game == "iba-5") {   // lesson 5
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-6") { // lesson 6
+			if (game == "iba-6") {   // lesson 6
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-7") { // lesson 7
+			if (game == "iba-7") {   // lesson 7
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-8") { // lesson 8
+			if (game == "iba-8") {   // lesson 8
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-9") { // lesson 9
+			if (game == "iba-9") {   // lesson 9
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-10") { // lesson 10
+			if (game == "iba-10") {   // lesson 10
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-r1") { // review v1
+			if (game == "iba-r1") {   // review v1
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-r2") { // review v2
+			if (game == "iba-r2") {   // review v2
 				error = "false";
 				return "000000";
 			} else
-			if (game == "iba-r3") { // review v3
+			if (game == "iba-r3") {   // review v3
 				error = "false";
 				return "000000";
 			}
@@ -163,7 +163,11 @@
 			if (game == "true") {
 				return "invalid";
 			}
+
+	}
 	
+	function play(cgame) {
+		parent.location.href = "redirect.html?game=" + game;
 	}
 	
 	var game;
