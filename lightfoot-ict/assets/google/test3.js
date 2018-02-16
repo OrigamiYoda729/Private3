23ac93f07eb91deb8436fb2bd8750302b057bb56
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-02-15 19:49:38 -0500

    update

---
+++ lightfoot-ict/assets/google/test3.js
@@ -0,0 +1,122 @@
+	
+	var auth2 = {};
+	var helper = (function() {
+		return {
+			onSignInCallback: function(authResult) {
+				$('#authResult').html('Auth Result:<br/>');
+				for (var field in authResult) {
+					$('#authResult').append(' ' + field + ': ' +
+						authResult[field] + '<br/>');
+				}
+				if (authResult.isSignedIn.get()) {
+					$('#main-content').show('slow');
+					$('#gConnect').hide();
+					helper.profile();
+					helper.people();
+				} else {
+					if (authResult['error'] || authResult.currentUser.get().getAuthResponse() == null) {
+						console.log('There was an error: ' + authResult['error']);
+					}
+					$('#authResult').append('Logged out');
+					$('#main-content').hide('slow');
+					$('#gConnect').show();
+				}
+				console.log('authResult', authResult);
+			},
+
+			disconnect: function() {
+				auth2.disconnect();
+			},
+
+			people: function() {
+				gapi.client.plus.people.list({
+					'userId': 'me',
+					'collection': 'visible'
+				}).then(function(res) {
+					var people = res.result;
+					$('#visiblePeople').empty();
+					$('#visiblePeople').append('Number of people visible to this app: ' +
+						people.totalItems + '<br/>');
+					for (var personIndex in people.items) {
+						person = people.items[personIndex];
+						$('#visiblePeople').append('<img src="' + person.image.url + '">');
+					}
+				});
+			},
+
+			profile: function() {
+				gapi.client.plus.people.get({
+					'userId': 'me'
+				}).then(function(res) {
+					var profile = res.result;
+					var displayName = profile.displayName;
+					console.log(profile);
+					$('#profile').empty();
+					$('#profile').append(
+						$('<p><img src=\"' + profile.image.url + '\"></p>'));
+					$('#profile').append(
+						$('<p>Hello ' + profile.displayName + '!<br />Tagline: ' +
+							profile.tagline + '<br />About: ' + profile.aboutMe + '</p>'));
+					if (profile.emails) {
+						$('#profile').append('<br/>Emails: ');
+						for (var i = 0; i < profile.emails.length; i++) {
+							$('#profile').append(profile.emails[i].value).append(' ');
+						}
+						$('#profile').append('<br/>');
+					}
+					if (profile.cover && profile.coverPhoto) {
+						$('#profile').append(
+							$('<p><img src=\"' + profile.cover.coverPhoto.url + '\"></p>'));
+					}
+				}, function(err) {
+					var error = err.result;
+					$('#profile').empty();
+					$('#profile').append(error.message);
+				});
+			}
+		};
+	})();
+
+	$(document).ready(function() {
+		$('#disconnect').click(helper.disconnect);
+		$('#loaderror').hide();
+		if ($('meta')[0].content == 'YOUR_CLIENT_ID') {
+			alert('This sample requires your OAuth credentials (client ID) ' +
+				'from the Google APIs console:\n' +
+				'    https://code.google.com/apis/console/#:access\n\n' +
+				'Find and replace YOUR_CLIENT_ID with your client ID.'
+			);
+		}
+	});
+
+	var updateSignIn = function() {
+		console.log('update sign in state');
+		if (auth2.isSignedIn.get()) {
+			console.log('signed in');
+			helper.onSignInCallback(gapi.auth2.getAuthInstance());
+		} else {
+			console.log('signed out');
+			helper.onSignInCallback(gapi.auth2.getAuthInstance());
+		}
+	}
+
+	function startApp() {
+		gapi.load('auth2', function() {
+			gapi.client.load('plus', 'v1').then(function() {
+				gapi.signin2.render('signin-button', {
+					scope: 'https://www.googleapis.com/auth/plus.login',
+					fetch_basic_profile: false
+				});
+				gapi.auth2.init({
+					fetch_basic_profile: false,
+					scope: 'https://www.googleapis.com/auth/plus.login'
+				}).then(
+					function() {
+						console.log('init');
+						auth2 = gapi.auth2.getAuthInstance();
+						auth2.isSignedIn.listen(updateSignIn);
+						auth2.then(updateSignIn);
+					});
+			});
+		});
+	}
