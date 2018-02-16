9ea6161b0a5cc398d9168f37ccdd9e546cf339b1
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-02-16 13:04:45 -0500

    update

--- lightfoot-ict/commit.bat
+++ lightfoot-ict/commit.bat
@@ -1,3 +1,5 @@
+git config --global user.email 33796301+OrigamiYoda729@users.noreply.github.com
+git config --global user.name OrigamiYoda729
 cd ../
 git init
 git pull https://github.com/OrigamiYoda729/Private3.git master
