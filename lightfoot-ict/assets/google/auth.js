aa9b8cb2c9014eb8ec8ca76e5e1f1ad02eb80a34
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-02-15 20:04:12 -0500

    update

--- lightfoot-ict/assets/google/auth.js
+++ lightfoot-ict/assets/google/auth.js
@@ -2,6 +2,7 @@
 	function onSignIn(googleUser) {
 		
 		var profile = googleUser.getBasicProfile();
+		console.log('Profile: ' + profile);
 		console.log('ID: ' + profile.getId());
 		console.log('Name: ' + profile.getName());
 		console.log('Image URL: ' + profile.getImageUrl());
