e81c8eb50860efa72fecdb6cb03cbb8ebf698c08
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-03-01 19:47:06 -0500

    Update: commit.bat, bug fixes

--- Burkalicious/installprinter.bat
+++
@@ -1,8 +0,0 @@
-@Echo Off
-REM Change \\COMPUTER\PRINTER to the printer you want to install
-
-REM Add printer
-rundll32 printui.dll,PrintUIEntry /in /n\\COMPUTER\PRINTER
-
-REM Set printer as default
-rundll32 printui.dll,PrintUIEntry /y /n\\COMPUTER\PRINTER
