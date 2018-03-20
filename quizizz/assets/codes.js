49438eaac95e06f9dd5a69cc0827e7e485c31559
OrigamiYoda729 <33796301+OrigamiYoda729@users.noreply.github.com>
2018-03-20 13:59:45 -0400

    Update codes.js

--- quizizz/assets/codes.js
+++ quizizz/assets/codes.js
@@ -5,7 +5,7 @@
 		// mutlimedia
 			if (game == "multimedia-1") {   // lesson 1
 				error = "false";
-				return "116793";
+				return "846379";
 			} else
 			if (game == "multimedia-2") {   // lesson 2
 				error = "false";
@@ -17,7 +17,7 @@
 			} else
 			if (game == "multimedia-r1") {   // lesson v1
 				error = "false";
-				return "097073";
+				return "663281";
 			} else
 			if (game == "multimedia-r2") {   // review v2
 				error = "false";
@@ -51,7 +51,7 @@
 			} else
 			if (game == "programming-r1") {   // lesson v1
 				error = "false";
-				return "669989";
+				return "869056";
 			} else
 			if (game == "programming-r2") {   // review v2
 				error = "false";
@@ -77,7 +77,7 @@
 			} else
 			if (game == "webdesign-r1") {   // lesson v1
 				error = "false";
-				return "563247";
+				return "695135";
 			} else
 			if (game == "webdesign-r2") {   // review v2
 				error = "false";
@@ -103,7 +103,7 @@
 			} else
 			if (game == "gaming-r1") {   // review v1
 				error = "false";
-				return "079466";
+				return "371615";
 			} else
 			if (game == "gaming-r2") {   // review v2
 				error = "false";
@@ -113,7 +113,7 @@
 		// iba
 			if (game == "learn-1") {   // lesson 1
 				error = "false";
-				return "291796";
+				return "415811";
 			} else
 			if (game == "learn-2") {   // lesson 2
 				error = "false";
@@ -157,45 +157,45 @@
 			} else
 			if (game == "learn-r2") {   // custom review
 				error = "false";
-				return "419918";
+				return "228852";
 			} else
 				
 		// extreme
 			if (game == "extreme-1") {   // part 1
 				error = "false";
-				return "933531";
+				return "743840";
 			} else
 			if (game == "extreme-2") {   // part 2
 				error = "false";
-				return "708032";
+				return "935445";
 			} else
 			if (game == "extreme-3") {   // part 3
 				error = "false";
-				return "878321";
+				return "955442";
 			} else
 			if (game == "extreme-4") {   // part 4
 				error = "false";
-				return "265333";
+				return "978968";
 			} else
 			if (game == "extreme-5") {   // part 5
 				error = "false";
-				return "07964";
+				return "137474";
 			} else
-			if (game == "extreme-6") {   // part 6 -- start
+			if (game == "extreme-6") {   // part 6
 				error = "false";
-				return "890993";
+				return "687855";
 			} else
 			if (game == "extreme-7") {   // part 7
 				error = "false";
-				return "067263";
+				return "262150";
 			} else
 			if (game == "extreme-8") {   // part 8
 				error = "false";
-				return "706354";
+				return "508702";
 			} else
-			if (game == "extreme-c1") {   // custom review
+			if (game == "extreme-c1") {   // full review
 				error = "false";
-				return "679223";
+				return "990512";
 			} else
 			
 			if (game == "true") {
@@ -205,30 +205,4 @@
 	
 	function play(cgame) {
 		parent.location.href = "redirect.html?game=" + cgame;
-	}
-	
-	function disable() {
-		var x;
-		var y;
-		var game;
-		var error;
-		var games = ["multimedia-1", "multimedia-2", "multimedia-3", "multimedia-r1", "multimedia-r2", "programming-1", "programming-2", "programming-3", "programming-4", "programming-5", "programming-6", "programming-r1", "programming-r2", "webdesign-1", "webdesign-2", "webdesign-3", "webdesign-4", "webdesign-r1", "webdesign-r2", "gaming-1", "gaming-2", 	"gaming-3", "gaming-4", "gaming-r1", "gaming-r2", "iba-1", "iba-2", "iba-3", "iba-4", "iba-5", "iba-6", "iba-7", "iba-8", "iba-9", "iba-10", "iba-r1", "iba-r2", "iba-r3"];
-		var subjects = ["multimedia", "programming", "webdesign", "gaming", "iba"];
-		var types = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "r1", "r2", "r3"];
-		
-		for (var i = 0; i < games.length; i++) {
-			for (var h = 0; h < subjects.length; h++) {
-				for (var j = 0; j < types.length; j++) {
-					x = subjects[h] + "-" +   types[j];
-					if (x == games[i]) {
-						y = getCode(x);
-						alert(x);
-						if (y == "000000") {
-							document.getElementById(x).className += " disabled";
-							document.getElementById(x).disabled = "true";
-						}
-					}
-				}
-			}
-		}
 	}
